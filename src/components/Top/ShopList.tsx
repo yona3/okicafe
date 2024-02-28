6ec8d0a482b81d987557cabec5c7b5ca6721c7ad
yona3 <yonajs208@gmail.com>
2024-02-28 14:56:13 +0900

    Topコンポーネントを追加

---
+++ src/components/Top/ShopList.tsx
@@ -0,0 +1,3 @@
+export const ShopList = () => {
+  return <div>shop list</div>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Top } from "@/components/Top";
 import { Layout } from "@/components/common/Layout";
 import Head from "next/head";
 
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Top page</div>
+      <Top />
     </Layout>
   );
 }
