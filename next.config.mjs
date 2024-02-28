b5f245abf81ec333a04feae550f1dc5c3d686dd4
yona3 <yonajs208@gmail.com>
2024-02-28 11:56:01 +0900

    api/shopsの追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const { VERCEL_URL, HOTPEPPER_API_KEY } = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    API_HOST: VERCEL_URL ? `https://${VERCEL_URL}` : "http://localhost:3000",
+  },
+  serverRuntimeConfig: {
+    HOTPEPPER_API_KEY,
+  },
 };
 
 export default nextConfig;
