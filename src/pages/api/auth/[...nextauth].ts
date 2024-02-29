bd43d58d0316e8913f9026e22e87df50c73aef74
yona3 <yonajs208@gmail.com>
2024-02-29 15:25:55 +0900

    setup db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,13 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextApiHandler } from "next";
+import prisma from "@/lib/prisma";
 
 const { GITHUB_ID, GITHUB_SECRET, SECRET } = process.env;
 
-export const authOptions = {
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -27,4 +31,8 @@ export const authOptions = {
     },
   },
 };
-export default NextAuth(authOptions);
+
+const authHandler: NextApiHandler = (req, res) =>
+  NextAuth(req, res, authOptions);
+
+export default authHandler;
