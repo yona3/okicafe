f256a7dfbd996256cb13efb516eb8beb661f18f7
yona3 <yonajs208@gmail.com>
2024-02-29 16:05:06 +0900

    不要なオプションの削除

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,20 +20,6 @@ const authOptions: AuthOptions = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
-  callbacks: {
-    async jwt({ token, account }: any) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }: any) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
 };
 
 const authHandler: NextApiHandler = (req, res) =>
